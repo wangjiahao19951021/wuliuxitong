66d75e794e013ada44abbf528e904286a7f8ff14
wangjiahao <196279741@qq.com>
2019-05-17 08:49:46 +0800

    1

---
+++ index.md
@@ -0,0 +1,5 @@
+#css css
+#dw  页面
+#lib 外部资源
+#js  js
+#PSD PSD
---
+++ js/common.js
@@ -0,0 +1,31 @@
+// 设置ie兼容ajax 此处jq-1.11.3
+jQuery.support.cors=true;
+// ajax公用设置
+var global_url = "http://www.chinacarbon-al.com"
+$.ajaxSetup({
+    url : global_url,
+    global : false,
+    type : 'post',
+    dataType : 'json',
+    cache : false,
+    timeout : 10000,
+    beforeSend : function() {
+	// 加载loading等操作
+	$.DialogByZ.Loading('../../lib/jQueryZdialog/image/loading.png'); 
+    },
+    complete : function() {
+	// 加载完毕后执行操作 删除loading等操作
+	$.DialogByZ.Close();
+    },
+    error : function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR, textStatus, errorThrown)
+	// 加载顺序原因此处弹窗需要延时触发
+	setTimeout(function () {
+	     $.DialogByZ.Alert({Title: "", Content: "系统链接超时，请稍后再试",BtnL:"确定",FunL:alerts});
+	}, 1)
+    }
+});
+// 关闭提示
+function alerts(){
+    $.DialogByZ.Close();
+}
